14b9d8ce3f994f15a83e11345016fe4efe916eb5
toonvd <toon_vd@live.com>
2021-08-13 16:55:06 +0200

    smaller fixes

--- src/MageHost/CheckPerformanceRows/VarnishHitrateRow.php
+++ src/MageHost/CheckPerformanceRows/VarnishHitrateRow.php
@@ -55,7 +55,7 @@ class VarnishHitrateRow extends AbstractRow
             'Average Varnish Hitrate',
             $status,
             round($hitrate * 100, 0) . '%',
-            '> 80%',
+            '>= 80%',
         );
     }
 }
